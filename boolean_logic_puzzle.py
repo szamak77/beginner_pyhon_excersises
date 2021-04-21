c5f03daead45cc7366b33afdc1cb9dccebcbe3da
szamak77 <szamak77@gmail.com>
2021-04-21 20:16:16 +0200

    Added source in comment

--- boolean_logic_puzzle.py
+++ boolean_logic_puzzle.py
@@ -1,3 +1,10 @@
+"""
+source:
+Brain Games Python
+99 Brain Teasers for Beginners to Energize
+Your Brain Cells and Python Logic Skills
+by Christian Mayer, Lukas Rieger, and Zohaib Riaz
+"""
 a, b = True, False
 out = (a and b) or (a and b and not a) or a
 print(out)
